861ed4dc96845411161654f8bc0a97b6557b94ee
Rainer Du <dusiyh@gmail.com>
2012-07-30 21:45:37 +0800

    fixed a but in test

--- test/diandian_oauth/interface_test.rb
+++ test/diandian_oauth/interface_test.rb
@@ -4,7 +4,7 @@ class InterfaceTest < ActiveSupport::TestCase
   CLIENT_ID="3zgJwues92"
   CLIENT_SECRET="1rxLZr5R4sweGUATGig25VBc1ka1xDEpkIqJ"
   ACCESS_TOKEN = {
-      :token => '9986f9cb-75d6-441a-8109-bbe9ddd1086b',
+      :access_token => '9986f9cb-75d6-441a-8109-bbe9ddd1086b',
       :refresh_token => "c27e5e5b-6837-48c5-8fa1-5eab4efc4158",
       :token_type => "bearer",
       :expires_in => 3599,
@@ -22,10 +22,10 @@ class InterfaceTest < ActiveSupport::TestCase
   test 'user_info' do
     client = self.client
     client.access_token= ACCESS_TOKEN
+    p client.access_token
     begin
       p client.user_info
     rescue DiandianOAuth::API::TokenExpiredException => e
-
     end
   end
 
